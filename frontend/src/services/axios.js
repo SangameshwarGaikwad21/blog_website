c9320721873faad86fa631d56abd3a9d28d952d6
gaikwadsangameshwar <sangamgaikwad214@gmail.com>
2026-01-14 22:10:34 +0530

    added the bacend url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:2000/api/v1",
+  baseURL: "https://sangam-blog.vercel.app/api/v1",
   withCredentials: true,
 });
 export default axiosInstance;
