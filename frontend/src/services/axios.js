39567fe4953101e11eaf24bd52a6c7e1cc6f2532
gaikwadsangameshwar <sangamgaikwad214@gmail.com>
2026-01-16 13:23:28 +0530

    fix: update post route and revert axios baseURL to hardcoded value

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_URL,
+  baseURL: "https://bloggs-01wb.onrender.com",
   withCredentials: true,
 });
 export default axiosInstance;
